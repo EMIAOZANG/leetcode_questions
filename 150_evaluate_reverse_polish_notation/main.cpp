1045c5f0c510bfed590920d282e185ec9894e8a2
EMIAOZANG <christopher.w.lu@gmail.com>
2016-01-17 18:22:42 -0500

    150 reverse polish

--- 150_evaluate_reverse_polish_notation/main.cpp
+++ 150_evaluate_reverse_polish_notation/main.cpp
@@ -1,3 +1,8 @@
+//reverse polish notation evaluation
+//use one stack for numbers, at each operator, get two numbers and compute the result, then push back to the number stack
+//
+//Time Complexity: O(n)
+//Space Complexity: O(n)
 #include <iostream>
 #include <vector>
 #include <string>
@@ -11,37 +16,35 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
       stack<int> numStack;
-      stack<string> opStack;
+      string p = "+-*/";
 
-      regex numP("\\d+");
-      regex opP("[+-*/]");
       for (int i = 0; i < tokens.size(); i++){
-        if (regex_match(tokens[i],numP)){
+          //is a operator
+        int num_1;
+        int num_2;
+        if (p.find(tokens[i]) != string::npos  && numStack.size() >= 2){
+          num_2 = numStack.top();
+          numStack.pop();
+          num_1 = numStack.top();
+          numStack.pop();
+        }
+        if (tokens[i] == "+"){
+          numStack.push(num_1+num_2); 
+        }
+        else if (tokens[i] == "-"){
+          numStack.push(num_1-num_2);
+        }
+        else if (tokens[i] == "*"){
+          numStack.push(num_1*num_2);
+        }
+        else if (tokens[i] == "/"){
+          numStack.push(num_1/num_2);
+        }
+        else{
+          //is a number
           numStack.push(stoi(tokens[i]));
         }
-        else if (regex_match(tokens[i],opP)){
-          //is a operator
-          int num_1;
-          int num_2;
-          if (numStack.size() >= 2){
-            num_2 = numStack.top();
-            numStack.pop();
-            num_1 = numStack.top();
-            numStack.pop();
-          }
-          if (tokens[i] == "+"){
-            numStack.push(num_1+num_2); 
-          }
-          else if (tokens[i] == "-"){
-            numStack.push(num_1-num_2);
-          }
-          else if (tokens[i] == "*"){
-            numStack.push(num_1*num_2);
-          }
-          else if (tokens[i] == "/"){
-            numStack.push(num_1/num_2);
-          }
-        }
+
         //else error
       }
       
--- 77_combinations/main.cpp
+++ 77_combinations/main.cpp
@@ -5,11 +5,30 @@ using namespace std;
 
 class Solution {
 public:
+    void search(vector<vector<int> >& result, vector<int>& curr, int start, const int n, const int k){
+      curr.push_back(start);
+        if (curr.size() == k){
+          result.push_back(curr);
+        } 
+        else {
+          for (int i = start+1; i <= n; i++){
+            search(result,curr,i,n,k);
+          }
+        }
+      curr.pop_back();
+    }
+    
     vector<vector<int> > combine(int n, int k) {
-      vector<int> () 
+      vector<int> temp;  
+      vector<vector<int> > res;
+      for (int i = 1; i <= n; i++){
+        search(res,temp,i,n,k);
+      }
+      return res;
     }
 };
 
 int main(){
+  Solution sls;
   return 0;
 }

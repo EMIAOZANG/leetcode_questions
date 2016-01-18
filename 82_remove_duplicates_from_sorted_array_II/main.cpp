5c48b7e517878cdbaf4a675484d84574bf9dc19c
EMIAOZANG <christopher.w.lu@gmail.com>
2016-01-17 19:38:57 -0500

    82 remove duplicates in linked list

--- 82_remove_duplicates_from_sorted_array_II/main.cpp
+++ 82_remove_duplicates_from_sorted_array_II/main.cpp
@@ -1,3 +1,9 @@
+//delete duplicates in linked list
+//
+//Boundary conditions:
+//1. fast->next == NULL
+//2. !head or !head->next
+//3.slow = NULL (when first non-duplicate number is not found)
 #include <iostream>
 using namespace std;
 
@@ -13,16 +19,39 @@ public:
         if ( head == NULL ){
           return NULL;
         }
-        ListNode* fast = head->next;
-        ListNode* slow = head;
-        while ( fast != NULL ){
-           if ( fast->val != slow->val ){
-             slow = slow->next;
-           } 
-           else{
-             //remove all nodes between the fast and slow pointer
+        ListNode* fast = head;
+        ListNode* slow = NULL;
+        bool extFlag = false;
+
+        while ( fast->next != NULL ){
+           if ( extFlag || fast->val == fast->next->val ){
+             //remove nodes
+             if ( slow == NULL ){
+               head = fast->next;
+             }
+             else {
+               slow->next = fast->next;
+             }
+             extFlag = true;
+             if ( fast-> val != fast->next->val ){
+               extFlag = false;
+             }
+           }
+           else {
+             slow = fast;
+             
            }
            fast = fast->next;
         }
+        if ( extFlag ){
+          //remove the last possible node
+          if ( slow == NULL ){
+            head = fast->next;
+          }
+          else {
+            slow->next = fast->next;
+          }
+        }
+        return head;
     }
 };

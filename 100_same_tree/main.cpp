443df85949f6ce264e2de731b4eccaf68ed8cf44
EMIAOZANG <christopher.w.lu@gmail.com>
2016-01-20 17:30:22 -0500

    same tree, easy recursion

---
+++ 100_same_tree/main.cpp
@@ -0,0 +1,25 @@
+//easy recursion tree comparison
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+      if (!p && !q){
+        return true;
+      }
+      if ((!p && q) || (!q && p) || (q->val != p->val)){
+        return false;
+      }
+
+      return (p->val == q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};

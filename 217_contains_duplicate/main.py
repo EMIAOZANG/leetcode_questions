c06d530a481d2dac19229786c2995e38b99270d1
EMIAOZANG <christopher.w.lu@gmail.com>
2016-04-17 18:05:49 -0400

    nice python 1liner

---
+++ 217_contains_duplicate/main.py
@@ -0,0 +1,11 @@
+"""
+Nice python 1-liner
+"""
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(set(nums)) != len(nums)
+        

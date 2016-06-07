ffdc9eb6b84ce69940de8043e1584bcc5ab744de
EMIAOZANG <christopher.w.lu@gmail.com>
2016-06-06 23:23:34 -0700

    220 duplicate III, bucket id and test case design ++++

--- 220_contains_duplicate_III/main.py
+++ 220_contains_duplicate_III/main.py
@@ -1,3 +1,24 @@
+"""
+Follow-up of contain duplicate II:
+    think of it as the exactly same case, in problem II we built a hashmap that memorizes all values appeared, now we want to build buckets with width t+1, then
+    If |nums[i] - nums[j]| <= t, it is either:
+    1) nums[i], nums[j] are in the same bucket
+    or
+    2) they are in adjacent bucket
+    we use a hashmap to save the (bucket_id, val) pair, bucket_id can be computed as nums[i] / (t + 1)
+
+    Note that we also need to remove overflowed elem at the end of each iteration to keep the size of sliding window unchanged
+
+    Time: O(N)
+    Space: O(k)
+
+
+    Test Cases:
+    1. []
+    2. [-1,-1] -- negs
+    3. [-1, 1] -- pos and negs
+    4. k <= 0 or t < 0
+"""
 class Solution(object):
     def containsNearbyAlmostDuplicate(self, nums, k, t):
         """
@@ -6,12 +27,27 @@ class Solution(object):
         :type t: int
         :rtype: bool
         """
-        idx_l = [(val,idx) for idx, val in enumerate(nums)]    
-        idx_l.sort()
-        print idx_l
+        if len(nums) <= 1 or t < 0 or k <= 0:
+            return False
+        d = {}
+        bwidth = t + 1
+        for i in xrange(len(nums)):
+            bid = nums[i] / bwidth #compute the bucket id
 
-        for i in xrange(1,len(idx_l)):
-           for i in  
+            if bid in d:    #two elems are in the same bucket
+                return True
+            if (bid - 1) in d and abs(nums[i] - d[bid - 1]) <= t:
+                return True
+            if (bid + 1) in d and abs(nums[i] - d[bid + 1]) <= t:
+                return True
+            if i >= k and nums[i - k] / bwidth in d :
+                del d[nums[i - k] / bwidth] #remove the first elem in the sliding window
+            d[bid] = nums[i] #add new bucket id
+        return False
+                
+            
+
+        
 sls = Solution()
 l = [1,3,6,2]
 print sls.containsNearbyAlmostDuplicate(l,1,2)
